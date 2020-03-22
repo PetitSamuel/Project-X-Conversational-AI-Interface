3d3f40880dc5273e2b97c77981087714f3537a0d
RuxinDing <rding@tcd.ie>
2020-03-22 02:14:16 +0000

    For adding new intents

--- training-interface/src/components/Assistants/Assistants.js
+++ training-interface/src/components/Assistants/Assistants.js
@@ -2,8 +2,53 @@ import React, { Component } from 'react';
 import Row from 'react-bootstrap/Row';
 import '../../App.css';
 
-
 class Assistants extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      Intent: '',
+      Description: ''
+    };
+  }
+
+  handleValueChange(field, value, type='string') {
+    if(type === 'number'){
+      value = + value;
+    }
+    this.setState({
+      [field]: value
+    });
+  }
+  handleSubmit (e) {
+    e.preventDefault();
+ 
+    const {Intent,Description} = this.state;
+    fetch('http://localhost:3000/assistants', {
+      method: 'post',
+      body: JSON.stringify({
+        Intent,
+          Description
+      }),
+        headers: {
+       'Content-Type': 'application/json'
+        }
+    })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.id) {
+        alert('add successfully');
+        this.setState({
+          Intent: '',
+          Description: ''
+        });
+      } else {
+        alert('fail to add');
+      }
+    })
+    .catch((err) => console.error(err));
+  }
+
 
   render () {
     
@@ -11,29 +56,32 @@ class Assistants extends Component {
     <div>
     <Row>
 
-        <div className ="tile1">
-            <h3 style ={{paddingBottom: "6px"}}>Assistants</h3>
-          <div className = "stat-box">
-            
-            
-          </div>   
-        </div>
-
-
         <div className = "tile2">
-      
+        <h4 style ={{paddingBottom: "6px"}}>Add New Intent</h4>
+            <form>
+            <label>Intent:</label>
+            <input type="text" />
+            <br />
+            <label>Description:</label>
+            <input type="text" />
+            
+            <br />
+            <input type="submit" value="submit" />
+          </form>
+
         </div>
 
-      </Row>
+    </Row>
 
-      <Row>
+    <Row>
 
       <div className = "tile3">
-       
+          <h3 style ={{paddingBottom: "6px"},{paddingLeft: "10px"}}>Intents</h3>
+          
       </div>
    
-      </Row> 
-      </div>
+    </Row> 
+    </div>
     )
   }
 }
