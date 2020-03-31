0a382ad56ef8b16191378a544c366de82eb6e38f
Seán Roche <35078186+senroche@users.noreply.github.com>
2020-03-31 14:27:07 +0100

    Assistant cards for final presentation

--- training-interface/src/components/Assistants/Assistants.js
+++ training-interface/src/components/Assistants/Assistants.js
@@ -1,27 +1,60 @@
 import React, { Component } from 'react';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import StatsCard from './components'
 import '../../App.css';
 
+import { TiTick } from "react-icons/ti";
+import { GiRobotAntennas } from "react-icons/gi";
+import { FaRobot } from "react-icons/fa";
+import { MdSmsFailed } from "react-icons/md";
 
-class Assistants extends Component {
 
+class Assistant extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      status: true,
+      activeSessions:0,
+      chatsServed:0,
+      failures:0,
+      
+    };
+  }
   render () {
     
     return (
     <div>
-    <Row>
-
-        <div className ="tile1">
-            <h3 style ={{paddingBottom: "6px"}}>Assistants</h3>
-          <div className = "stat-box">
-            
-            
-          </div>   
-        </div>
-   
-      </Row> 
+          <Row style = {{paddingTop:"20px"}}>
+            <Col lg={3} sm={6}>
+              <StatsCard
+                bigIcon={<FaRobot />}
+                statTitle="OpenJawTest1"
+                statValue={this.state.status ? 'Up' : 'Down'}
+                statIcon={<TiTick />}
+                statIconText="Current"
+              />
+            </Col>
+            <Col lg={3} sm={6}>
+              <StatsCard
+                bigIcon={<GiRobotAntennas />}
+                statTitle="CustomerSupport"
+                statValue={this.state.activeSessions}
+                statIcon={""}
+                statIconText="Last updated, 2 days ago"
+              />
+            </Col>
+            <Col lg={3} sm={6}>
+              
+              />
+            </Col>
+            <Col lg={3} sm={6}>
+           
+            </Col>
+          </Row>
+ 
       </div>
     )
   }
 }
-export default Assistants;
+export default Assistant;
