b3ac19aea6d1396197c1cffc15a5352c643cd58e
sam <petits@tcd.ie>
2020-04-08 17:02:44 +0200

    adding router file and moving routing to that file

---
+++ server/router.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const db_api_controller = require('./Api/db_api');
+const db_helper_api_controller = require('./Api/db_helper_api');
+const converter_api_controller = require('./Api/converter_api');
+
+router.post('/intents', db_api_controller.post_intents);
+router.post('/intents-generation', db_helper_api_controller.generate_intents);
+router.post('/entities', db_api_controller.post_entities);
+router.post('/entities-generation', db_helper_api_controller.generate_entities);
+// router.post('/api/dialogs', db_api_controller.post_dialogs);
+router.post('/upload-csv', converter_api_controller.post_upload_csv);
+
+router.get('/intents', db_api_controller.get_intents);
+router.get('/intents-analytics', db_helper_api_controller.get_intents_analytics);
+router.get('/intents/:name', db_api_controller.get_intents);
+router.get('/entities', db_api_controller.get_entities);
+router.get('/entities-analytics', db_helper_api_controller.get_entities_analytics);
+router.get('/entities/:name', db_api_controller.get_entities);
+// router.get('/api/dialogs', db_api_controller.get_dialogs);
+router.get('/download-md/:filename', converter_api_controller.get_download_md);
+
+router.delete('/intents/:name', db_api_controller.remove_intent);
+router.delete('/entities/:name', db_api_controller.remove_entity);
+
+exports.router = router;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const cors = require('cors')
-var router = express.Router();
-var db_api_controller = require('./Api/db_api');
-var db_helper_api_controller = require('./Api/db_helper_api');
-var converter_api_controller = require('./Api/converter_api');
-require('dotenv').config()
+const router = require('./router').router;
+const dotenv = require('dotenv');
 
+dotenv.config();
 if (!process.env.SERVER_PORT ||
     !process.env.RASA_ENDPOINT ||
     !process.env.MONGO_CONNECTION_STRING ||
@@ -21,37 +19,14 @@ const app = express()
 app.use(cors());
 app.use(express.json());
 app.use(require("body-parser").json())
-
 app.get('/', (req, res) => {
-    res.status(200).send("Hey");
+    res.status(200).send("Welcome");
 });
-
-router.post('/intents', db_api_controller.post_intents);
-router.post('/intents-generation', db_helper_api_controller.generate_intents);
-router.post('/entities', db_api_controller.post_entities);
-router.post('/entities-generation', db_helper_api_controller.generate_entities);
-// router.post('/api/dialogs', db_api_controller.post_dialogs);
-router.post('/upload-csv', converter_api_controller.post_upload_csv);
-
-router.get('/intents', db_api_controller.get_intents);
-router.get('/intents-analytics', db_helper_api_controller.get_intents_analytics);
-router.get('/intents/:name', db_api_controller.get_intents);
-router.get('/entities', db_api_controller.get_entities);
-router.get('/entities-analytics', db_helper_api_controller.get_entities_analytics);
-router.get('/entities/:name', db_api_controller.get_entities);
-// router.get('/api/dialogs', db_api_controller.get_dialogs);
-router.get('/download-md/:filename', converter_api_controller.get_download_md);
-
-router.delete('/intents/:name', db_api_controller.remove_intent);
-router.delete('/entities/:name', db_api_controller.remove_entity);
-
 app.use('/api', router);
-
 // all other endpoints are 404s
 app.use(function (req, res, next) {
     res.status(404).send('Not found');
 });
-
 app.listen(process.env.SERVER_PORT, () => console.log('Server started on ' + process.env.SERVER_PORT));
 
 exports.app = app;
